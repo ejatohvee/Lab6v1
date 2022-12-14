2b6c5314bc13db9b51e2147f016d7d550c40b2a4
ejatohvee <113019609+ejatohvee@users.noreply.github.com>
2022-12-14 11:48:45 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ class testCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.subtract(10, 8), 2)
 
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(7, 5), 35)
+        self.assertEqual(self.calculator.multiply(7, 5), 1230)
 
     def test_divide(self):
         self.assertEqual(self.calculator.divide(10, 2), 5)
